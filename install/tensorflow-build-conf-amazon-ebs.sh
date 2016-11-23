63245c5f60d73c42b5ecfbf244644e4726296a6b
Gil Raphaelli <gil@realscout.com>
2016-11-22 21:23:12 -0500

    sync CUDA version for tf build

--- install/tensorflow-build-conf-amazon-ebs.sh
+++ install/tensorflow-build-conf-amazon-ebs.sh
@@ -2,7 +2,7 @@
 export CUDA_TOOLKIT_PATH=/usr/local/cuda
 export CUDNN_INSTALL_PATH=/usr/local/cuda
 export TF_NEED_CUDA=1
-export TF_CUDA_VERSION=7.5
+export TF_CUDA_VERSION=8.0
 export TF_CUDNN_VERSION=4
 export TF_CUDA_COMPUTE_CAPABILITIES=3.0
 export TF_UNOFFICIAL_SETTING=1
